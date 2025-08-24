1438bae8a27d892117268464bf9a733fe8c12ba2
asifhas2 <ak1247942@gmail.com>
2025-08-24 12:09:24 +0600

    some change and add togling feture

--- js/addMoney.js
+++ js/addMoney.js
@@ -20,3 +20,41 @@ document.getElementById("addMoneyBtn").addEventListener("click",function(e){
    const allAmount = addAmount + amountValue;
    document.getElementById("amount").innerText = allAmount;
 })
+
+// cash out feture ****************
+
+document.getElementById("cashOutMoneyBtn").addEventListener("click",function(e){
+    e.preventDefault()
+    const aggentNumber =document.getElementById("aggentNumbers").value;
+    const ammount = parseInt(document.getElementById("amounts").value);
+    const pinNumberss = parseInt(document.getElementById("pins").value);
+     const amountValue = parseInt(document.getElementById("amount").innerText);
+
+      if(aggentNumber.length<11){
+    alert("account number do not mach");
+    return;
+   }
+   if(pinNumberss !== pinNumbers){
+    alert("enter your right pin number")
+    return;
+   }
+
+     const allAmount = amountValue - ammount;
+     document.getElementById("amount").innerText = allAmount;
+     
+    
+})
+
+// togling ///////////////////
+
+document.getElementById("add money").addEventListener("click",function(e){
+    e.preventDefault()
+    document.getElementById("add-money-pearent").style.display="block";
+    document.getElementById("cash-out-parent").style.display="none";
+})
+document.getElementById("cash-out").addEventListener("click",function(e){
+    e.preventDefault()
+    document.getElementById("cash-out-parent").style.display="block";
+    document.getElementById("add-money-pearent").style.display="none";
+    
+})
